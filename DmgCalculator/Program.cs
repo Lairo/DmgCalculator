8ccab8048f794266170a35139f73089bd754ecb4
chajus1 <chajus1@googlemail.com>
2024-09-21 03:10:19 +0100

    Sword damage + 123

--- DmgCalculator/Program.cs
+++ DmgCalculator/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            SwordDamage swordDamage = new();
+
+            while (true)
+            {
+                
+                Console.WriteLine($"0 for no magic/flaming, 1 for magic, 2 for " +
+                    $"flaming, 3 for both, anything else to quit: ");
+                char key = Console.ReadKey().KeyChar;
+                if ((key != '1') & (key != '2') & (key != '3')) return;
+                Console.WriteLine();
+            }
         }
     }
 }
