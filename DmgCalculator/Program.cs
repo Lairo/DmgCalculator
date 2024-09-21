93e86a3c0fe2ae4b3254b33fa52aeb5c0505efcc
chajus1 <chajus1@googlemail.com>
2024-09-21 01:29:37 +0100

    Add project files.

---
+++ DmgCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DmgCalculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DmgCalculator/SwordDamage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DmgCalculator
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+    }
+}
