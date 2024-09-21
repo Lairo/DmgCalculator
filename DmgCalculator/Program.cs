30d7f66e1bb710d365e31259cd87f529f93d3735
chajus1 <chajus1@googlemail.com>
2024-09-21 23:03:15 +0100

    done?

--- DmgCalculator/Program.cs
+++ DmgCalculator/Program.cs
@@ -9,11 +9,21 @@
             while (true)
             {
                 
-                Console.WriteLine($"0 for no magic/flaming, 1 for magic, 2 for " +
+                Console.Write($"0 for no magic/flaming, 1 for magic, 2 for " +
                     $"flaming, 3 for both, anything else to quit: ");
-                char key = Console.ReadKey().KeyChar;
-                if ((key != '1') & (key != '2') & (key != '3')) return;
-                Console.WriteLine();
+                char key = Console.ReadKey(false).KeyChar;
+                if ((key != '0') & (key != '1') & (key != '2') & (key != '3')) return;
+
+                swordDamage.Roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7) +
+                    Random.Shared.Next(1, 7);
+                if (key == '1' || key == '3') swordDamage.SetMagic(true);
+                else swordDamage.SetMagic(false);
+                if (key == '2' || key == '3') swordDamage.SetFlaming(true);
+                else swordDamage.SetFlaming(false);
+                Console.WriteLine($"\nRolled {swordDamage.Roll} " +
+                    $"for {swordDamage.Damage} HP");
+
+
             }
         }
     }
