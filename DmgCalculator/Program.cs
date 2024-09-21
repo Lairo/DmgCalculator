6a6fa41a428f377ab9c51d57ae635d86af00d50a
chajus1 <chajus1@googlemail.com>
2024-09-21 23:06:54 +0100

    cleaner

--- DmgCalculator/Program.cs
+++ DmgCalculator/Program.cs
@@ -13,15 +13,12 @@
                     $"flaming, 3 for both, anything else to quit: ");
                 char key = Console.ReadKey(false).KeyChar;
                 if ((key != '0') & (key != '1') & (key != '2') & (key != '3')) return;
-
-                swordDamage.Roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7) +
+                int roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7) +
                     Random.Shared.Next(1, 7);
-                if (key == '1' || key == '3') swordDamage.SetMagic(true);
-                else swordDamage.SetMagic(false);
-                if (key == '2' || key == '3') swordDamage.SetFlaming(true);
-                else swordDamage.SetFlaming(false);
-                Console.WriteLine($"\nRolled {swordDamage.Roll} " +
-                    $"for {swordDamage.Damage} HP");
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine($"\nRolled {roll} for {swordDamage.Damage} HP");
 
 
             }
